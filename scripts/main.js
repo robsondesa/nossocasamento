bf119744035044a5872bff29dcf2f5a002392f20
melodesa101 <165803269+melodesa101@users.noreply.github.com>
2024-04-02 10:43:30 -0300

    Firt commit

---
+++ scripts/main.js
@@ -0,0 +1,125 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function() {
+  AOS.init({
+    // uncomment below for on-scroll animations to played only once
+    // once: true
+  }); // initialize animate on scroll library
+});
+
+// Smooth scroll for links with hashes
+$("a.smooth-scroll").click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        2000,
+        function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        }
+      );
+    }
+  }
+});
+
+// Photo Filter
+var activeFilter = "all";
+
+$(".ww-filter-button").on("click", function(e) {
+  // remove btn-primary from all buttons first
+  $(".ww-filter-button").removeClass("btn-primary");
+  $(".ww-filter-button").addClass("btn-outline-primary");
+
+  // add btn-primary to active button
+  var button = $(this);
+  button.removeClass("btn-outline-primary");
+  button.addClass("btn-primary");
+  filterItems(button.data("filter"));
+  e.preventDefault();
+});
+
+function filterItems(filter) {
+  if (filter === activeFilter) {
+    return;
+  }
+
+  activeFilter = filter;
+  $(".ww-gallery .card").each(function() {
+    var card = $(this);
+    var groups = card.data("groups");
+    var show = false;
+    if (filter === "all") {
+      show = true;
+    } else {
+      for (var i = 0; i < groups.length; i++) {
+        if (groups[i] === filter) {
+          show = true;
+        }
+      }
+    }
+    // hide everything first
+    card.fadeOut(400);
+    setTimeout(function() {
+      if (show && !card.is(":visible")) {
+        card.fadeIn(400);
+      }
+    }, 500);
+  });
+}
+
+// Defina a data do casamento (ano, mês (zero indexado), dia)
+var weddingDate = new Date(2025, 4, 21);
+
+function updateCountdown() {
+  var now = new Date();
+  var timeDifference = weddingDate - now;
+  
+  // Verifique se o casamento já ocorreu
+  if (timeDifference <= 0) {
+    document.getElementById('countdown').innerHTML = 'O casamento já ocorreu!';
+  } else {
+    var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    
+    document.getElementById('countdown').innerHTML = days + 'D ' + hours + 'H ' + minutes + 'M ' + seconds + 'S';
+  }
+}
+
+// Atualize a contagem regressiva a cada segundo
+setInterval(updateCountdown, 1000);
+
+// Fechar o menu após clicar em um item do menu
+$('.navbar-nav>li>a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
+});
+
+
+// Light Box
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
